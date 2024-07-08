dbb27f0f3ed311ea50c5d2a2c28c8c958d2c8ed7
Julian Gil <nailujgil@gmail.com>
2024-07-08 00:04:32 -0600

    Primera version

---
+++ app.js
@@ -0,0 +1,69 @@
+
+
+function funcion_encriptar()  {
+    let   texto = document.getElementById("texto-ingresado").value;
+    
+  if (comparatexto(texto)) {
+    alert("El texto contiene Mayusculas O Numeros ")
+      }
+
+      else{  
+         let textoremplazado = texto
+         .replace(/e/g,"enter")
+         .replace(/i/g,"imes")
+         .replace(/a/g,"ai")
+         .replace(/o/g,"ober")
+         .replace(/u/g,"ufat");
+         procesa_texto(textoremplazado);
+
+      }
+}
+
+function comparatexto(texto){
+
+return (/[A-Z]/.test(texto) || /\d/.test(texto) ||  /[!@#\$%\^\&*\)\(+=._-]/.test(texto) ) ;
+
+}
+
+function procesa_texto(textoremplazado){
+
+  document.getElementById("prestacion-encritador").style.display = "none";
+  document.getElementById("texto-encritado").style.display = "block";
+  asignarTextoElemento('.parrafo',`${textoremplazado}`); 
+
+}
+
+function copiar(){
+
+  let textocopiado = document.querySelector('.parrafo').innerText;
+
+  
+  navigator.clipboard.writeText(textocopiado).then(() => {
+    alert("El Texto se Copio correctamente");
+  }).catch(err => {
+    alert("No se copio nada",err);
+  });
+
+  }
+
+
+
+function funcion_desencriptar(){
+      textoinical = document.getElementById("texto-ingresado").value;
+    asignarTextoElemento('.parrafo',`${textoinical}`); 
+
+}
+
+function asignarTextoElemento(elemento, texto) {
+    let  elementoHTML  = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+ 
+}
+function condionesIniciales() {
+    asignarTextoElemento('.titulo','a');
+
+}
+
+
+
+condionesIniciales();
